2106a52dc8a6be9b7c33aa965b558cbe7425f46e
Tr <tr.thambi@gmail.com>
2017-02-06 21:02:56 +0000

    Update Bootstrap-ubuntu16.sh

--- CloudLog/Bootstrap-ubuntu16.sh
+++ CloudLog/Bootstrap-ubuntu16.sh
@@ -8,6 +8,6 @@ sudo apt-get install python -y
 sudo python ./awslogs-agent-setup.py --region $REGION --non-interactive --configfile "https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/BasicConfig"
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/awslogs.service
 cd /etc/systemd/system
-
+wget https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/awslogs.service
 sudo chmod +X /etc/systemd/system/awslogs.service
 sudo systemctl start awslogs.service
