b750cde6c6e7c8acc37909d849f6ed326d68bf8a
Tr <tr.thambi@gmail.com>
2017-02-06 20:57:39 +0000

    Create Bootstrap-ubuntu16.sh

---
+++ CloudLog/Bootstrap-ubuntu16.sh
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+REGION=$(wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+sudo curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+sudo chmod +x ./awslogs-agent-setup.py
+sudo apt-get install python -y
+#sudo apt-get install python-pip -y
+sudo python ./awslogs-agent-setup.py --region $REGION --non-interactive --configfile "https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/BasicConfig"
+
+sudo chmod +X /etc/systemd/system/awslogs.service
+sudo systemctl start awslogs.service
