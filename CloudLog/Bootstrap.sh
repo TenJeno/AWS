94031441e770ab934bae082df54b2aa10ce15aac
Tr <tr.thambi@gmail.com>
2017-02-06 19:02:26 +0000

    Update Bootstrap.sh

--- CloudLog/Bootstrap.sh
+++ CloudLog/Bootstrap.sh
@@ -1,6 +1,7 @@
 curl https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
+REGION=$(wget -q -O - http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
 sudo apt-get install python2.7 -y
 sudo apt-get install python-pip -y
-sudo python ./awslogs-agent-setup.py --region eu-west-1 --non-interactive --configfile="https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/BasicConfig"
+sudo python ./awslogs-agent-setup.py --region $REGION --non-interactive --configfile="https://raw.githubusercontent.com/BrokenFlame/AWS/master/CloudLog/BasicConfig"
 sudo chkconfig awslogs on
 sudo service awslogs start
