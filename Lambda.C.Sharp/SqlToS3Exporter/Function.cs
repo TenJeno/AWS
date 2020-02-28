a56f2c1756374b449bc0351761fbc08f5c033ce0
Tr <tr.thambi@gmail.com>
2020-02-28 06:00:25 +0000

    Update Function.cs

--- Lambda.C.Sharp/SqlToS3Exporter/Function.cs
+++ Lambda.C.Sharp/SqlToS3Exporter/Function.cs
@@ -4,12 +4,17 @@ using Amazon.Lambda.Serialization.Json;
 using Amazon.S3;
 using Amazon.S3.Model;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.IO;
 using System.Data;
 using MySql.Data.MySqlClient;
-
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
 
 namespace SqlToS3Exporter
 {
@@ -59,121 +64,104 @@ namespace SqlToS3Exporter
         /// <returns></returns>
         public static string FunctionHandler(dynamic input, ILambdaContext context)
         {
-            string qryTravelLocation = @"
-            SELECT tid `location_id`, geo_level,`en-gb` `location_name`	
-                FROM cms.ten_cache_location;
-            ";
-            string qryHotels = @"
-            SELECT DISTINCT
-                 n.nid `accommodation_id`
-			    ,nfpr.field_propertyreferenceid_value `ivector_id`
-			    ,nfd.title `accommodation_name`
-			    ,IFNULL(ttfd.name, '') `rating`
-			    ,cplv.property_latitude `latitude`
-			    ,cplv.property_longitude `longitude`
-			    ,cplv.location_tid `location_id`
-			    ,IFNULL(nfbi.field_benefit_included_value, 0) `is_benefits_hotel`
-		    FROM cms.node n
-            JOIN cms.node_field_data nfd
-                ON n.nid = nfd.nid
-                    AND n.langcode = nfd.langcode
-                    AND n.type = nfd.type
-            JOIN cms.ten_cache_property_location_vicinity cplv
-                ON n.nid = cplv.property_nid
-            JOIN cms.node__field_propertyreferenceid nfpr
-                ON n.nid = nfpr.entity_id
-            LEFT JOIN cms.node__field_star_rating nfsr
-                ON n.nid = nfsr.entity_id
-                    AND n.langcode = nfsr.langcode
-                    AND n.type = nfsr.bundle
-            LEFT JOIN cms.taxonomy_term_field_data ttfd
-                ON nfsr.field_star_rating_target_id = ttfd.tid
-                    AND ttfd.default_langcode = 1
-                    AND ttfd.`status` = 1
-            LEFT JOIN cms.node__field_benefit_included nfbi
-                ON n.nid = nfbi.entity_id
-                    AND n.langcode = nfbi.langcode
-                    AND n.type = nfbi.bundle
-            WHERE nfd.`status` = 1
-                AND n.type = 'accommodation'
-                AND cplv.vicinity = 1;
-            ";
 
-           
-            var queries = new Dictionary<string, string>();
-            queries.Add("CMS/Travel_Locations.sql", qryTravelLocation);
-            queries.Add("CMS/Hotels.sql", qryHotels);
+            bool bStatus = true;
+            int queriesCompleted = 0;
+            var envVariables = System.Environment.GetEnvironmentVariables();
+            string OutBucket = envVariables.Contains("OutBucket") ? envVariables["OutBucket"].ToString() : string.Empty;
+            string PrefixKey = envVariables.Contains("PrefixKey") ? envVariables["PrefixKey"].ToString() : string.Empty;
+            string QueryPrefixKey = envVariables.Contains("QueryPrefixKey") ? envVariables["QueryPrefixKey"].ToString() : string.Empty;
+            string QuerySourceBucket = envVariables.Contains("QuerySourceBucket") ? envVariables["QuerySourceBucket"].ToString() : string.Empty;
+            string AWS_DEFAULT_REGION = envVariables.Contains("AWS_DEFAULT_REGION") ? envVariables["AWS_DEFAULT_REGION"].ToString() : string.Empty;
+            string AccountNumber = envVariables.Contains("AccountNumber") ? envVariables["AccountNumber"].ToString() : string.Empty;
 
-            const string bucket = "<<BucketName>>";
 
-            foreach (KeyValuePair<string, string> query in queries)
-            {
-                try
-                {
-                    UploadToS3(context, bucket, String.Format("{0}", query.Key), query.Value.ToStream(), false);
-                }
-                catch (Exception ex)
-                {
-                    context.Logger.LogLine(string.Format("An error occured writing the query {0}, to bucket {1}"
-                            , query.Key, bucket));
-                    context.Logger.LogLine(ex.ToString());
-                }
-                
-            }
+            string Server = envVariables.Contains("Server") ? envVariables["Server"].ToString() : string.Empty;
+            string User = envVariables.Contains("User") ? envVariables["User"].ToString() : "master";
+            string Database = envVariables.Contains("Database") ? envVariables["Database"].ToString() : string.Empty;
+            string ServerPort = envVariables.Contains("ServerPort") ? envVariables["ServerPort"].ToString() : "3306";
+
+            string UserPassword = envVariables.Contains("UserPassword") ? envVariables["UserPassword"].ToString() : string.Empty; ;
+
+
+
+
+            string CsvDelimiter = envVariables.Contains("CsvDelimiter") ? envVariables["CsvDelimiter"].ToString() : ",";
+
+            var queryfiles = from keyNames in GetQueriesKeysFromBucket(QuerySourceBucket, QueryPrefixKey) where keyNames.EndsWith(".sql") select keyNames;
+            Dictionary<string, string> queries = GetQueriesFromBucket(QuerySourceBucket, queryfiles.ToList());
+            int totalNoQueries = queries.Count;
+            string datetime = DateTime.UtcNow.ToString("yyyyMMddhhmmss");
+            string failedQueries = "";
+
+            //foreach (KeyValuePair<string, string> query in queries)
+            //{
+            //    try
+            //    {
+            //        UploadToS3(context, OutBucket, String.Format("{0}", query.Key), query.Value.ToStream(), false);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        context.Logger.LogLine(string.Format("An error occured writing the query {0}, to bucket {1}"
+            //                , query.Key, QuerySourceBucket));
+            //        context.Logger.LogLine(ex.ToString());
+            //    }
+            //}
 
 
             foreach (KeyValuePair<string, string> query in queries)
             {
+                string queryName = query.Key.Split("/").Last();
                 DataTable tempTable = new DataTable();
+                tempTable.Reset();
 
-                string connStr = "server=" +
-                    "endpoint;" +
-                    "user=master;database=databasename;" +
-                    "port=3306;password=password";
+                string connStr = string.Format("server=" +
+                    "{0};" +
+                    "user={1};database={2};" +
+                    "port={3};password=<<Password>>", Server, User, Database, ServerPort);
                 using (MySqlConnection conn = new MySqlConnection(connStr))
                 {
                     try
                     {
+                        string fileName = Regex.Replace(queryName, "\\.sql$", ".csv");
+                        
                         conn.Open();
                         MySqlCommand cmd = new MySqlCommand(query.Value, conn);
                         MySqlDataReader rdr = cmd.ExecuteReader();
                         tempTable.Load(rdr);
+
+                        UploadToS3(context
+                            , OutBucket
+                            , string.Format("{0}/{1}/{2}", PrefixKey, datetime, fileName)
+                            , tempTable.ToCSV("|").ToStream()
+                            );
+
+                        queriesCompleted++;
                     }
                     catch (Exception ex)
                     {
                         context.Logger.LogLine(string.Format("An error occured executing the query {0}"
                             , query.Key));
                         Console.WriteLine(ex.ToString());
+                        bStatus = false;
+                        failedQueries = string.Join(",", failedQueries, queryName);
                     }
-
-                    UploadToS3(context
-                        , bucket
-                        , query.Key
-                        , tempTable.ToCSV().ToStream()
-                        , true);
                 }
             }
 
-            return "OK";
+            if(!bStatus)
+            {
+                return string.Format("{0} : {1}/{2} queries completed \n The following queries failed: {3}", bStatus.ToString(), queriesCompleted, totalNoQueries, failedQueries);
+            }
+            return string.Format("{0} : {1}/{2} queries completed", bStatus.ToString(), queriesCompleted, totalNoQueries);
 
         }
 
         public static bool UploadToS3(ILambdaContext context, string bucketName
             , string key
-            , Stream stream
-            , bool appendDate
-            , string fileExtension = ".csv")
+            , Stream stream)
         {
-            string datetime = DateTime.UtcNow.ToString("yyyyMMddhhmmss");
-            string keyName = String.Empty;
-            if (appendDate)
-            {
-                keyName = string.Format("{0}_{1}{2}", key, datetime, fileExtension);
-            }
-            else
-            {
-                keyName = key;
-            }
+            string keyName = key;
 
             try
             {
@@ -181,7 +169,7 @@ namespace SqlToS3Exporter
                 {
                     BucketName = bucketName,
                     Key = keyName,
-                    ContentType = "text/plain"
+                    ContentType = "text/csv"
                 };
                 putRequest.InputStream = stream;
                 var S3Client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest1);
@@ -190,11 +178,67 @@ namespace SqlToS3Exporter
             catch (Exception ex)
             {
                 context.Logger.LogLine(ex.Message);
-                context.Logger.LogLine(String.Format("Unable to write to file {0}, to bucket {1}.", keyName , bucketName));
+                context.Logger.LogLine(String.Format("Unable to write to file {0}, to bucket {1}."
+                    , keyName , bucketName));
                 context.Logger.LogLine(ex.ToString());
                 return false;
             }
             return true;
+        }
+
+        public static List<string> GetQueriesKeysFromBucket(string bucket, string keyPrefix)
+        {
+            List<string> queriesKeys = new List<string>();
+            AmazonS3Client client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest1);
+            ListObjectsRequest listRequest = new ListObjectsRequest
+            {
+                BucketName = bucket,
+                Prefix = keyPrefix
+            };
+
+            ListObjectsResponse listResponse;
+            do
+            {
+                listResponse = client.ListObjectsAsync(listRequest).Result;
+                foreach (S3Object obj in listResponse.S3Objects)
+                {
+                    queriesKeys.Add(obj.Key);
+                }
+
+                listRequest.Marker = listResponse.NextMarker;
+            } while (listResponse.IsTruncated);
+            return queriesKeys;
+        }
+
+        public static Dictionary<string, string> GetQueriesFromBucket(string bucket
+            , List<string> keys)
+        {
+            var S3Client = new AmazonS3Client(Amazon.RegionEndpoint.EUWest1);
+            Dictionary<string, string> files = new Dictionary<string, string>();
+            foreach (string key in keys) 
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucket,
+                    Key = key
+                };
+                using (StreamReader streamReader = new StreamReader(
+                    S3Client.GetObjectAsync(request).Result.ResponseStream))
+                {
+                    files.Add(key, streamReader.ReadToEnd());
+                }
+            }
+            return files;
+        }
+         
+
+        private static string GetPassword(string SecretId)
+        {
+            var secretManagerClient = new AmazonSecretsManagerClient(Amazon.RegionEndpoint.EUWest1);
+            GetSecretValueRequest request = new GetSecretValueRequest();
+            request.SecretId = SecretId;
+            var response = secretManagerClient.GetSecretValueAsync(request).Result;
+            return response.SecretString;
         }
     }
 }
